da6cbe0b09e2a0c4cd9b5d9950aa7eafea55996f
arunsingh111 <arunsingh33331@gmail.com>
2021-04-28 19:41:27 +0530

    initial commit

---
+++ @elegantstack/education/.cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,17 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/education/.cache/dev-404-page.js")),
+  "component---packages-blog-gatsby-blog-core-src-templates-collection-author-js": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-core/src/templates/collection.author.js")),
+  "component---packages-blog-gatsby-blog-core-src-templates-collection-category-js": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-core/src/templates/collection.category.js")),
+  "component---packages-blog-gatsby-blog-core-src-templates-collection-tag-js": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-core/src/templates/collection.tag.js")),
+  "component---packages-blog-gatsby-blog-core-src-templates-post-js": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-core/src/templates/post.js")),
+  "component---packages-blog-gatsby-blog-core-src-templates-posts-js": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-core/src/templates/posts.js")),
+  "component---packages-blog-gatsby-blog-pages-src-pages-404-jsx": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-pages/src/pages/404.jsx")),
+  "component---packages-blog-gatsby-blog-pages-src-pages-authors-jsx": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-pages/src/pages/authors.jsx")),
+  "component---packages-blog-gatsby-blog-pages-src-pages-contact-jsx": preferDefault(require("/home/arun/Documents/SuperAgencyBlog/@elegantstack/packages/blog/gatsby-blog-pages/src/pages/contact.jsx"))
+}
+
